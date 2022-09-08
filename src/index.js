a680080811576115192f7e16b217f431c6298774
Franciane <franciane.penina@gmail.com>
2022-09-08 03:40:08 -0300

    initializing project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Teste"</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
